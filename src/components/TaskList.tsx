a61056e39b17d1bf08802c97d68fe3c66c35f5f0
Roger Crescencio <rogersenefaria@gmail.com>
2021-03-28 14:47:31 -0300

    Entrega do primeiro desafio do Ignite

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,34 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks(oldState => [...oldState,newTask]);
+    setNewTaskTitle('')
+
+
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task => {
+      if(id === task.id) {
+        task.isComplete = !task.isComplete
+      } return task
+    })
+    setTasks(newTasks)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const  filterTaskId = tasks.filter(task =>  task.id !== id)
+    setTasks(filterTaskId)
   }
 
   return (
